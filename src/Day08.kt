c078fbb750b5a5314fb919a5a9948a072df322a8
Mfon <mfon.lwo.udoh@gmail.com>
2024-12-23 19:38:28 +0000

    Day 8 p2

--- src/Day08.kt
+++ src/Day08.kt
@@ -6,17 +6,17 @@ fun main() {
             val nodes = antennas
                 .filterIndexed { index, _ -> index != i }
                 .map { it.translate(antennas[i]) }
-                .filter{ it.validateCoordinate(matrixSize) }
+                .filter { it.validateCoordinate(matrixSize) }
             antinodes.addAll(nodes)
         }
         return antinodes
     }
 
     fun part1(input: List<String>): Int {
-        val matrix = input.map{ it.toCharArray().toList() }
+        val matrix = input.map { it.toCharArray().toList() }
         val antennas = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
-        for(i in matrix.indices) {
-            for(j in matrix[i].indices) {
+        for (i in matrix.indices) {
+            for (j in matrix[i].indices) {
                 if (matrix[i][j] != '.') {
                     val frequency = antennas.getOrDefault(matrix[i][j], mutableListOf())
                     frequency.add(Pair(i, j))
@@ -31,12 +31,28 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val matrix = input.map { it.toCharArray().toList() }
+        val antennas = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+        for (i in matrix.indices) {
+            for (j in matrix[i].indices) {
+                if (matrix[i][j] != '.') {
+                    val frequency = antennas.getOrDefault(matrix[i][j], mutableListOf())
+                    frequency.add(Pair(i, j))
+                    antennas.put(matrix[i][j], frequency)
+                }
+            }
+        }
+
+        val antinodes = antennas
+            .map { getAntinodes2(it.value, matrix.size) }
+            .flatten()
+            .toSet()
+        return antinodes.size
     }
 
     val testInput = readInput("Day08_test")
     check(part1(testInput) == 14)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 34)
 
     val input = readInput("Day08")
     part1(input).println()
@@ -51,4 +67,24 @@ private fun Pair<Int, Int>.translate(other: Pair<Int, Int>): Pair<Int, Int> {
     val diffY = other.first - this.first
     val diffX = other.second - this.second
     return Pair(other.first + diffY, other.second + diffX)
+}
+
+fun getAntinodes2(antennas: MutableList<Pair<Int, Int>>, matrixSize: Int): MutableList<Pair<Int, Int>> {
+    val antinodes = mutableListOf<Pair<Int, Int>>()
+    for (i in antennas.indices) {
+        antennas
+            .filterIndexed { index, _ -> index != i }
+            .map { node -> antinodes.add(node); node }
+            .map { getNodes(antennas[i], it, matrixSize, antinodes) }
+    }
+    return antinodes
+}
+
+fun getNodes(prevNode: Pair<Int, Int>, currentNode: Pair<Int, Int>, matrixSize: Int, container: MutableList<Pair<Int, Int>>): MutableList<Pair<Int, Int>> {
+    val newNode = prevNode.translate(currentNode)
+    if (newNode.validateCoordinate(matrixSize)) {
+        container.add(newNode)
+        return getNodes(currentNode, newNode, matrixSize, container)
+    }
+    return container
 }
